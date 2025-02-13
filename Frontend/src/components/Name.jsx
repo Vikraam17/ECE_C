3bd1e226d5c0a17d75918c54294c49dcce58b77b
Vikraam17 <vikyvino1773@gmail.com>
2025-02-13 12:48:36 +0530

    State and Event Handling added

--- Frontend/src/components/Name.jsx
+++ Frontend/src/components/Name.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const Name = () => {
+const Name = (props) => {
   return (
     <div>
-      
+      <h1>Name : {props.name}</h1>
+      <h1>Last Name : {props.lname}</h1>
+      <h1>Phone no : {props.phone}</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Name
+export default Name;
---
+++ javascript/terinaryOperator.js
@@ -0,0 +1,6 @@
+let a=6;
+
+let result = a%2===0 ? "Even" : "Odd";
+
+
+console.log(result)
